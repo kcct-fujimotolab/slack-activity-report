9e24d80a5e3738fd1abf2f4cf5bf30e7a9e1af01
Hayato Kawai <fohte.hk@gmail.com>
2016-10-17 12:42:43 +0900

    Fix "No module named activityreport.slackbot"

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='slack-activity-report',
       version='1.0.0',
       url='https://github.com/kcct-fujimotolab/slack-activity-report',
-      packages=['activityreport'],
+      packages=['activityreport', 'activityreport.slackbot'],
       entry_points={
           'console_scripts': [
               'slack-activity-report = activityreport.main:main'
