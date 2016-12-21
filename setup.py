a940822cbae7c1f1ee5733140be764a86a4205a1
Hayato Kawai <fohte.hk@gmail.com>
2016-12-21 14:50:52 +0900

    Fix not found activityreport/slackbot

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='slack-activity-report',
       version='1.0.0',
       url='https://github.com/kcct-fujimotolab/slack-activity-report',
-      packages=['activityreport', 'activityreport.slackbot'],
+      packages=['activityreport'],
       entry_points={
           'console_scripts': [
               'slack-activity-report = activityreport.main:main'
