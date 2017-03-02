66c5b1e390f8969605cd9733c5175cb1b357e921
beybr6 <sdookdrp@gmail.com>
2017-03-03 00:42:26 +0900

    follow coding styles

--- activityreport/formatter.py
+++ activityreport/formatter.py
@@ -2,70 +2,56 @@ import datetime
 import math
 
 
-def _element_to_string(dateTimeList_element):
+def _element_to_string(date_time_list_element):
     # convert datetime.* to string
 
-    def _aDayOfWeek_en_to_ja(dateTime):
-        if dateTime.strftime("%A") == "Monday":
-            return "月"
-        elif dateTime.strftime("%A") == "Tuesday":
-            return "火"
-        elif dateTime.strftime("%A") == "Wednesday":
-            return "水"
-        elif dateTime.strftime("%A") == "Thursday":
-            return "木"
-        elif dateTime.strftime("%A") == "Friday":
-            return "金"
-        elif dateTime.strftime("%A") == "Saturday":
-            return "土"
-        elif dateTime.strftime("%A") == "Sunday":
-            return "日"
+    def _a_day_of_week_en_to_ja(date_time):
+        return ['月', '火', '水', '木', '金', '土', '日'][date_time.weekday()]
 
-    for (i, element) in enumerate(dateTimeList_element):
+    for i, element in enumerate(date_time_list_element):
 
         if isinstance(element, datetime.datetime):
-            # format: "2017/01/01(月) 00:00:00"
-            dateTimeList_element[i] = element.strftime(
-                "%Y/%m/%d(" + _aDayOfWeek_en_to_ja(element) + ") %H:%M:%S")
+            # format: '2017/01/01(月) 00:00:00'
+            date_time_list_element[i] = element.strftime(
+                '%Y/%m/%d(' + _a_day_of_week_en_to_ja(element) + ') %H:%M:%S')
         elif isinstance(element, datetime.date):
-            # format: "2017/01/01(月)"
-            dateTimeList_element[i] = element.strftime(
-                "%Y/%m/%d(" + _aDayOfWeek_en_to_ja(element) + ")")
+            # format: '2017/01/01(月)'
+            date_time_list_element[i] = element.strftime(
+                '%Y/%m/%d(' + _a_day_of_week_en_to_ja(element) + ')')
         elif isinstance(element, datetime.time):
-            # format: "00:00:00"
-            dateTimeList_element[i] = element.strftime("%H:%M:%S")
+            # format: '00:00:00'
+            date_time_list_element[i] = element.strftime('%H:%M:%S')
 
-    return dateTimeList_element
+    return date_time_list_element
 
 
-def _retGFM(dateTimeList):
-    transposedDateTimeList = list(map(list, zip(*dateTimeList)))
+def _ret_gfm(date_time_list):
+    transposed_date_time_list = list(map(list, zip(*date_time_list)))
 
-    gfm = ""
-    for x in dateTimeList:
-        gfm += "|"
+    gfm = ''
+    for x in date_time_list:
+        gfm += '|'
         for j, y in enumerate(x):
             if isinstance(y, int):
                 # get maximum number of digits
-                maxNumberOfDigits = int(math.log10(
-                    max(transposedDateTimeList[j])) + 1)
-                gfm += str(y).rjust(maxNumberOfDigits, ' ')  # align right
-                gfm += "|"
+                max_number_of_digits = len(str(max(transposed_date_time_list[j])))
+                gfm += str(y).rjust(max_number_of_digits, ' ')  # align right
+                gfm += '|'
             else:
                 # get length of maximum length str
-                maxStrLength = len(max(transposedDateTimeList[j], key=len))
-                gfm += str(y).ljust(maxStrLength, ' ')  # align left
-                gfm += "|"
+                max_str_length = len(max(transposed_date_time_list[j], key=len))
+                gfm += str(y).ljust(max_str_length, ' ')  # align left
+                gfm += '|'
 
-        gfm += "\n"
+        gfm += '\n'
 
     return gfm
 
 
-def to_gfm_table(dateTimeList):
+def to_gfm_table(date_time_list):
     # convert tuple into list
-    for i, element in enumerate(dateTimeList):
-        dateTimeList[i] = list(dateTimeList[i])
-        dateTimeList[i] = _element_to_string(dateTimeList[i])
+    for i, element in enumerate(date_time_list):
+        date_time_list[i] = list(date_time_list[i])
+        date_time_list[i] = _element_to_string(date_time_list[i])
 
-    _retGFM(dateTimeList)
+    return _ret_gfm(date_time_list)
